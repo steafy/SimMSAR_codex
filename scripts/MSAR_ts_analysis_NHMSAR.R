4a5d69a2f20bc887c0549678a6200e075eb78e5e
Stefan <beckerse@students.uni-marburg.de>
2026-06-17 11:13:25 +0200

    Set actual values

--- scripts/MSAR_ts_analysis_NHMSAR.R
+++ scripts/MSAR_ts_analysis_NHMSAR.R
@@ -20,16 +20,16 @@ source("R/estimation/estimate_MSAR.R")
 # -----------------------------------------------------------------------------
 # Network Structure Parameters
 # -----------------------------------------------------------------------------
-N <- c(6)                  # Number of nodes in the network
+N <- c(4, 6, 8)                  # Number of nodes in the network
 
-Density <- c(0.25)   # Network edge density (proportion of possible edges)
+Density <- c(0.25, 0.5, 0.75)   # Network edge density (proportion of possible edges)
                               # Range: 0 to 1
                               # Can be a vector: c(0.2, 0.5, 0.8)
 
 # -----------------------------------------------------------------------------
 # Regime Parameters
 # -----------------------------------------------------------------------------
-M <- c(1)               # Number of regimes (network states)
+M <- c(1, 2, 3, 4)               # Number of regimes (network states)
                               # Can be a vector: c(2, 3, 4)
 
 remain_lower <- 0.33          # Lower bound for probability to stay in same regime
@@ -49,7 +49,7 @@ max_edg_val <- 1              # Maximum absolute edge weight in networks
 # -----------------------------------------------------------------------------
 # Time Series Parameters
 # -----------------------------------------------------------------------------
-T <- c(2000)            # Number of time steps per time series
+T <- c(100, 200, 500, 1000, 1500, 2000, 2500, 3000, 3500)            # Number of time steps per time series
                               # Can be a vector: c(1000, 3500, 5000)
  
 warmup <- 50                  # Number of warmup time steps (discarded from analysis)
@@ -57,7 +57,7 @@ warmup <- 50                  # Number of warmup time steps (discarded from anal
 
 totTime <- T + warmup         # Total time steps including warmup
 
-n_ts <- 1                    # Number of time series to generate per condition
+n_ts <- 30                    # Number of time series to generate per condition
                               # Higher = more statistical power but slower
 
 # -----------------------------------------------------------------------------
